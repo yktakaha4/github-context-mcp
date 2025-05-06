8d5f8212461cb5a3b4b1be19b5793b216cc8e720
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2025-05-06 21:52:18 +0900

    add

--- src/github.ts
+++ src/github.ts
@@ -46,6 +46,39 @@ export const searchIssues = async (repoInfo: GitHubRepoInfo, keyword: string) =>
   return response;
 }
 
+export const searchPullRequests = async (repoInfo: GitHubRepoInfo, keyword: string) => {
+  const octokit = await getOktoKitClient();
+  const { owner, repo } = repoInfo;
+  const response = await octokit.paginate(octokit.rest.pulls.list, {
+    owner,
+    repo,
+    q: keyword,
+  });
+  return response;
+}
+
+export const getIssueComments = async (repoInfo: GitHubRepoInfo, issueNumber: number) => {
+  const octokit = await getOktoKitClient();
+  const { owner, repo } = repoInfo;
+  const response = await octokit.paginate(octokit.rest.issues.listCommentsForRepo, {
+    owner,
+    repo,
+    pull_number: issueNumber,
+  });
+  return response;
+}
+
+export const getPullRequestReviewComments = async (repoInfo: GitHubRepoInfo, pullRequestNumber: number) => {
+  const octokit = await getOktoKitClient();
+  const { owner, repo } = repoInfo;
+  const response = await octokit.paginate(octokit.rest.pulls.listReviewCommentsForRepo, {
+    owner,
+    repo,
+    pull_number: pullRequestNumber,
+  });
+  return response;
+}
+
 const getOktoKitClient = async () => {
   const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
